c4998dae81f57646e0b615a0020f8c48058f740d
ojab <ojab@ojab.ru>
2014-05-05 15:06:52 +0400

    Fix starting unicorn under :unicorn_user

--- lib/capistrano/unicorn/utility.rb
+++ lib/capistrano/unicorn/utility.rb
@@ -40,7 +40,7 @@ module CapistranoUnicorn
     # Check if a remote process exists using its pid file
     #
     def remote_process_exists?(pid_file)
-      test("[ -e #{pid_file} ]") && execute("#{try_unicorn_user} kill -0 `cat #{pid_file}` > /dev/null 2>&1")
+      test("[ -e #{pid_file} ]") && execute("#{try_unicorn_user.join(' ')} kill -0 `cat #{pid_file}` > /dev/null 2>&1")
     end
 
     # Stale Unicorn process pid file
@@ -77,7 +77,7 @@ module CapistranoUnicorn
     #
     def unicorn_send_signal(signal, pid=get_unicorn_pid)
       sig_prefix = Integer === signal ? '-' : '-s '
-      execute try_unicorn_user, 'kill', sig_prefix, signal, pid
+      execute *try_unicorn_user, 'kill', sig_prefix, signal, pid
     end
 
     # Run a command as the :unicorn_user user if :unicorn_user is a string.
@@ -85,9 +85,9 @@ module CapistranoUnicorn
     #
     def try_unicorn_user
       if unicorn_user = fetch(:unicorn_user)
-        "sudo -u #{unicorn_user}"
+        [:sudo, '-Eu', unicorn_user]
       else
-        ''
+        []
       end
     end
 
@@ -126,7 +126,7 @@ module CapistranoUnicorn
 
       within fetch(:app_path) do
         with rails_env: fetch(:rails_env), bundle_gemfile: fetch(:bundle_gemfile) do
-          execute :bundle, 'exec', fetch(:unicorn_bin), '-c', unicorn_config_file_path, '-E', fetch(:unicorn_rack_env), '-D', fetch(:unicorn_options)
+          execute *try_unicorn_user, :bundle, 'exec', fetch(:unicorn_bin), '-c', unicorn_config_file_path, '-E', fetch(:unicorn_rack_env), '-D', fetch(:unicorn_options)
         end
       end
     end
